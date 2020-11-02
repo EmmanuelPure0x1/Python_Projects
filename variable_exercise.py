8d0b7fae9ae1ec1fb38be81864edddf3401a821b
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-11-02 18:46:12 +0000

    Python Variables

---
+++ variable_exercise.py
@@ -0,0 +1,13 @@
+#Create 3 Variables to get user data
+
+#name
+name = input("What is your name? > ")
+
+#DOB
+dob = input("What is your DOB? > ")
+
+#age
+age = input("Please enter age: ")
+
+print("Hi", name, "is am  correct to believe you are born on the following date? > " + dob,
+      "and you are", age + ".")
