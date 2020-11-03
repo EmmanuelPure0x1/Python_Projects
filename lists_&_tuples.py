514636049795bfbf7d046ca2bba9e0652c729ebf
EmmanuelPure0x1 <eadejobi@spartaglobal.com>
2020-11-03 11:43:16 +0000

    lists & tuples

---
+++ lists_&_tuples.py
@@ -0,0 +1,62 @@
+# collection in Python
+# Lists
+# ADD, REMOVE, CHANGE are all possible within python lists.
+# Lists are mutable
+# Syntax: ["bread","eggs","milk"]
+
+#LIST DEFINITON: A List is an array of data held within []
+
+#ctrl + / to cmment everything out
+
+# MANAGING LISTS
+
+# #Let's create a list
+# shopping_list = ["apple", "milk", "bread"]
+#
+# # printing a list
+# print(shopping_list)
+#
+# #finding out type of variable
+# print(type(shopping_list))
+#
+# # look at indexing in the list items
+# print(shopping_list[1])
+#
+# # printing bread from list
+# print(shopping_list[2])
+#
+# # printing milk and bread (slicing)
+# print(shopping_list[1:2])
+#
+# # printing the last item
+# print(shopping_list[-1])
+#
+# # add item to list using append
+# shopping_list.append("cheese")
+# print(shopping_list)
+#
+# # adding an item to the front of the list
+# shopping_list.append("banana")
+# print(shopping_list)
+#
+# # removing an item from our list
+# shopping_list.remove("banana")
+# print(shopping_list)
+#
+# # how to remove an item from the end of the list
+# shopping_list.pop()
+# print(shopping_list)
+#
+# # How can I replace an item in my list
+# shopping_list[1] = "fruits"
+# print(shopping_list)
+
+# can we have mixed data types in the list?
+mixed_shopping_list = [1,2,3,"apple", "milk", "bread"]
+print(mixed_shopping_list)
+
+#Task Create a mixed data type list pf 7 items
+# display the type of the data
+# add, delete, replace, pop,
+# use indexing to print the list in reverse order
+
